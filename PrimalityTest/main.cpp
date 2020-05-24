f8afefeda8ab76234c14aadcb363c034863e1a6e
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-05-24 00:33:31 -0400

    Added functionality to list all prime numbers for inputted number

--- PrimalityTest/main.cpp
+++ PrimalityTest/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 /**
  * test if number is prime or not
  * 
- * @param n {int} user's inputted number
+ * @param n {int} a positive int above 1
  * @return {bool} true is prime, false otherwise
  */
 bool is_prime(int n) {
@@ -28,6 +28,25 @@ bool is_prime(int n) {
     }
 
     return true;
+}
+
+/**
+ * list primes up to arg
+ * 
+ * @param n {int} a positive int above 1
+ * @return {string} list of prime numbers
+ */
+string prime_list(int n) {
+    string prime_list = "";
+
+    for (int i = 2; i <= n; i++) {
+        if (is_prime(i)) {
+            prime_list += to_string(i);
+            prime_list += " ";
+        }
+    }
+
+    return prime_list;
 }
 
 int main(int argc, char** argv) {
@@ -41,11 +60,18 @@ int main(int argc, char** argv) {
 
         if (n == 0 || n == 1) {
             calculating = false;
+            break;
+        } else if (n < 0) {
+            cout << "Cannot calculate primality for negative number" << endl;
+            continue;
         } else if (is_prime(n)) {
             cout << n << " is a prime number!!!" << endl;
         } else {
             cout << n << " is not a prime number..." << endl;
         }
+
+        cout << "Primes up to this number: " << endl;
+        cout << prime_list(n) << endl;
     }
 
     return 0;
